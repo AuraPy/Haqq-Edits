01103ac952d1af66b1abdb24bdfdf8d63fce2781
Zayan Arshad <zayanarshad88@gmail.com>
2024-08-17 19:09:54 +0100

    Added recitation prompt and ayah prompt

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pixabay.core
 import requests
+from pygame import mixer
+import time
 import random
 import yt_dlp
 import os
+
+def choose_sound():
+    sound = input("Please specify the path of the recitation you have chosen:\n\n")
+    mixer.music.load(sound)
+
+    return sound
 
 def choose_video(px, ayah):
     resultandquery = send_query(px, ayah)
     result = resultandquery[0]
     query = resultandquery[1]
     if len(result) > 0:
-        print(f"Found {len(result)} results")
+        print(f"\n\nFound {len(result)} results\n\n")
         for index in result:
             index.download(f"{query}.mp4", "medium")
             os.system(f"mpv '{query}.mp4'")
@@ -17,7 +25,7 @@ def choose_video(px, ayah):
                 break
             else:
                 os.system(f"rm '{query}.mp4'")
-                if input("\n\nWould you like to try another query? [y/n]") == "y":
+                if input("Would you like to try another query? [y/n]") == "y":
                     return choose_video(px, ayah)
         return f"{query}.mp4"
 
@@ -27,16 +35,19 @@ def choose_video(px, ayah):
 
 
 def send_query(px, ayah):
-    query = input(f"\n\nWhat query should I send for this ayah:\n\n{ayah}\n\n")
+    query = input(f"What query should I send for this ayah:\n\n{ayah}\n\n")
     result = pixabay_searcher(px, query)
 
     return result, query
 
 def ayah_retriever():
-    surah = random.randint(70, 114)
+    surahandayahs = input("Enter the range of ayahs this recitation has in the format '[surah]:[ayah]-[ayah]'\n\n")
+    surah, ayahrange = surahandayahs.split(":")
+    ayah1, ayah2 = ayahrange.split("-")
+    index = 0
     ayahs = []
 
-    for i in range(1, requests.get(f"http://api.alquran.cloud/v1/surah/{surah}/en.asad").json()["data"]["numberOfAyahs"]):
+    for i in range(int(ayah1), int(ayah2)):
         ayahs.append(requests.get(f"http://api.alquran.cloud/v1/ayah/{surah}:{i}/en.asad").json()["data"]["text"])
 
     return ayahs
@@ -47,10 +58,12 @@ def pixabay_searcher(pixabay, query: str):
     return search_videos
 
 def main():
+    player = mixer.init()
     px = pixabay.core("45287472-297041ba3024ea4a8882277b8")
     retrieved_ayahs = ayah_retriever()
-    ayahs_ok = input(f"The randomly selected ayahs are:\n\n{retrieved_ayahs}\n\nIs this ok? [y/n]").lower()
+    ayahs_ok = input(f"The selected ayahs are:\n\n{retrieved_ayahs}\n\nIs this ok? [y/n]").lower()
     files = []
+    start_of_ayahs = []
 
     if ayahs_ok == "n":
         main()
@@ -58,23 +71,26 @@ def main():
         print("I'm gonna take that as a no :D")
         main()
     else:
+        sound = choose_sound()
+        epoch = time.time()
+        for ayah in retrieved_ayahs:
+            if len(start_of_ayahs) > 0:
+                mixer.music.play(start=start_of_ayahs[len(start_of_ayahs)-1])
+            else:
+                mixer.music.play()
+            input(f"Press enter when the start of the following ayah is recited: {ayah}")
+            start_of_ayahs.append(time.time()-epoch)
+            print(start_of_ayahs)
+            mixer.music.stop()
         for ayah in retrieved_ayahs:
             video = choose_video(px, ayah)
             files.append(video)
 
     for file in files:
         os.system(f"rm '{file}'")
+    print(start_of_ayahs)
 
+    mixer.music.unload(sound)
 
-"""
-format = {
-    "outtmpl": "/home/zayan/Documents/Python_Projects/Haqq-Edits/%(id)s.mp4",
-    "quiet": True,
-    "no_warnings": True
-}
-
-yt = yt_dlp.YoutubeDL(format)
-yt.download(url_list=urls)
-"""
 
 main()
